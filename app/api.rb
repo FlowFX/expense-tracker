a65ca5f400e3c2250db10c8777be6b24d2859ecd
Florian Posdziech <hallo@flowfx.de>
2019-03-13 09:19:27 +0100

    Add some happy unit tests

--- app/api.rb
+++ app/api.rb
@@ -1,11 +1,25 @@
+require_relative 'ledger'
 require 'sinatra/base'
 require 'json'
 
 module ExpenseTracker
   # The base class
   class API < Sinatra::Base
+    def initialize(ledger: Ledger.new)
+      @ledger = ledger
+      super()
+    end
+
     post '/expenses' do
-      JSON.generate(expense_id: 42)
+      expense = JSON.parse(request.body.read)
+      result = @ledger.record(expense)
+
+      if result.success?
+        JSON.generate(expense_id: result.expense_id)
+      else
+        status 422
+        JSON.generate('error' => result.error_message)
+      end
     end
 
     get '/expenses/:date' do
