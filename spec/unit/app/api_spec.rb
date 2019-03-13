7f604ca150c35b748941161223e6fafab3029a12
Florian Posdziech <hallo@flowfx.de>
2019-03-13 09:26:11 +0100

    Refactor

--- spec/unit/app/api_spec.rb
+++ spec/unit/app/api_spec.rb
@@ -23,9 +23,7 @@ module ExpenseTracker
 
         it 'returns the expense id' do
           post '/expenses', JSON.generate(expense)
-
-          parsed = JSON.parse(last_response.body)
-          expect(parsed).to include('expense_id' => 417)
+          expect(parsed_response).to include('expense_id' => 417)
         end
 
         it 'responds with a 200 (OK)' do
@@ -45,9 +43,7 @@ module ExpenseTracker
 
         it 'returns an error message' do
           post '/expenses', JSON.generate(expense)
-
-          parsed = JSON.parse(last_response.body)
-          expect(parsed).to include('error' => 'Expense incomplete')
+          expect(parsed_response).to include('error' => 'Expense incomplete')
         end
 
         it 'responds with a 422 (Unprocessable entity)' do
@@ -55,6 +51,10 @@ module ExpenseTracker
           expect(last_response.status).to eq(422)
         end
       end
+    end
+
+    def parsed_response
+      JSON.parse(last_response.body)
     end
   end
 end
